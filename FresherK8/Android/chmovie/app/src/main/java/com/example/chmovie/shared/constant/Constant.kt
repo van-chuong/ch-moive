ee40b32ed2074bc5be624ad2a07b89b25a2213ab
Van Chuong <134tutu431@gmail.com>
2024-05-13 00:43:30 +0700

    handle sign out and username

--- FresherK8/Android/chmovie/app/src/main/java/com/example/chmovie/shared/constant/Constant.kt
+++ FresherK8/Android/chmovie/app/src/main/java/com/example/chmovie/shared/constant/Constant.kt
@@ -1,10 +1,10 @@
 package com.example.chmovie.shared.constant
 
 object Constant {
-    const val BASE_URL:String = "https://api.themoviedb.org/3/"
-    const val API_KEY:String = "7c6b66fa46a753be33c4d6d5030f06b8"
-    const val POSTER_BASE_URL:String = "https://image.tmdb.org/t/p/w500/"
-    const val SESSION_KEY:String = "session_id"
-    const val USERNAME_KEY:String = "session_id"
-    const val FAVORITE_DATABASE:String = "FAVORITE_DATABASE"
+    const val BASE_URL: String = "https://api.themoviedb.org/3/"
+    const val API_KEY: String = "7c6b66fa46a753be33c4d6d5030f06b8"
+    const val POSTER_BASE_URL: String = "https://image.tmdb.org/t/p/w500/"
+    const val SESSION_KEY: String = "session_id"
+    const val USERNAME_KEY: String = "username"
+    const val FAVORITE_DATABASE: String = "FAVORITE_DATABASE"
 }
